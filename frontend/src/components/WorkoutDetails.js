b8b04482920f2e57ba4dcd3e45d066a5ad3b15f2
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2023-01-22 13:50:42 -0600

    adding files

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const WorkoutDetails = ({ workout }) => {
+  const [error, setError] = useState(null)
+ const { dispatch } = useWorkoutsContext()
+  const {user} = useAuthContext()
+
+  const handleClick = async () => {
+
+    if (!user) {
+      setError('You mush be logged in!')
+      return;
+    }
+
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE',
+      headers : {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default WorkoutDetails
